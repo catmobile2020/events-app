5f5daa12458c7fa0c8b6e26792c19597dd37ce5c
mahmoudmostafa1993 <mahmoud.moustafa@cat.com.eg>
2023-06-22 08:05:46 +0300

    add layout and multi lang and header in homepage

---
+++ components/homepage/brief.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="brief-components">
+    <v-container>
+      <v-row>
+         <v-col md="6">
+
+         </v-col>
+         <v-col md="6">
+
+         </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
---
+++ components/homepage/header.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="headerComponent">
+    <overNavComponent />
+    <div class="header-content">
+      <img src="../../assets/Photos/home/home_header.png" alt="">
+      <v-container>
+        <div class="text">
+        <h1>
+          The Leading Real Estate Management Software in KSA
+        </h1>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit voluptatem aliquid esse sunt, sapiente velit blanditiis voluptatum similique eveniet architecto voluptatibus rerum optio adipisci commodi necessitatibus iste laboriosam. Aperiam, at?
+        </p>
+        <v-btn rounded class="gradiant">
+          Learn More
+        </v-btn>
+      </div>
+      </v-container>
+
+    </div>
+  </div>
+</template>
+<script>
+import overNavComponent from '../global/overNav.vue'
+
+export default {
+  components: {
+    overNavComponent
+  },
+};
+</script>
